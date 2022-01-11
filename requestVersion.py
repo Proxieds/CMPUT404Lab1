3e1c80c43a7095267cd96642624690ac1f85cb99
Victor Lieu <victorlieu@Victors-MacBook-Air.local>
2022-01-10 18:13:02 -0700

    First Commit

---
+++ requestVersion.py
@@ -0,0 +1,12 @@
+# CMPUT 404 Lab 1
+
+import requests
+# Part 1: Print out the version of the requests library
+print("Requests Version: " + requests.__version__)
+
+# Part 2: Modify python script to make a GET Response to Google's homepage
+URL = "http://google.com"
+r = requests.get(url = "http://google.com")
+print("Google Response:",r)
+
+# Part 3: Modify Python script so that it downloads itself from GitHub and prints out its own source code from GitHub.
